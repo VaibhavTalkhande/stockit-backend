91e8d6099432b89301baef918ad231e4eb89bd23
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2025-07-09 13:19:01 +0530

    fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -50,7 +50,7 @@ const loginUser = async(req,res)=>{
         
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production', // true for HTTPS in production
+            secure: false, // true for HTTPS in production
             sameSite: 'lax',
             maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
         });
