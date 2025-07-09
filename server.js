2066258defe0e6ae8e71d6b96d91d1dbb01d39e0
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2025-07-09 13:52:32 +0530

    cors fix

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (!endpointSecret) {
 }
 connectDB();
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://stockit-wine.vercel.app/"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
