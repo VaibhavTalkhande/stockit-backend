433e36b447a819fbf8c8862b43c0723f6265c025
vaibhav Talkhande <49303222+VaibhavTalkhande@users.noreply.github.com>
2025-07-27 14:37:11 +0530

    Update salesController.js

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -63,8 +63,8 @@ export const createSale = async (req,res)=>{
                 quantity: p.quantity,
             })),
             //only needed if using dashboard
-            // success_url: `${process.env.FRONTEND_URL}/`,
-            // cancel_url: `${process.env.FRONTEND_URL}/cancel`,
+             success_url: `${process.env.FRONTEND_URL}/`,
+             cancel_url: `${process.env.FRONTEND_URL}/cancel`,
             metadata: { saleId: sale._id.toString() }
         });
 
